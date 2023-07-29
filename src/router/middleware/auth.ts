de19fcf6e99b08fafd7c9f653a3b920716fe0e31
BZLOM4JOPbI <prostoyachelovek@gmail.com>
2023-07-30 02:02:41 +0300

    fix: middleware types ignore

--- src/router/middleware/auth.ts
+++ src/router/middleware/auth.ts
@@ -1,7 +1,7 @@
 import { useUserStore, } from "@/stores/user"
 
 
-const authMiddleware = (to: { meta: { forAuth: any; forGuest: any; }; }, from: any, next: (arg0?: any) => void) => {
+const authMiddleware = (to: any, from: any, next: any) => {
     const userStore = useUserStore();
 
     const isAuth = userStore.token;
