ac0a776c81be48d3743f7724a53d6bd1678fffb4
BZLOM4JOPbI <prostoyachelovek@gmail.com>
2023-08-26 21:23:08 +0300

    add: profile head prototype

---
+++ src/components/layout/profile/ProfileAvatar.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps<{
+    imgUrl: string,
+}>();
+</script>
+
+<template>
+    <div class="avatar">
+        <div class="avatar__inner">
+            <div 
+                class="avatar__img"
+                :style="`background-image: url(${imgUrl})`"
+            >
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="sass">
+@import "@/assets/styles/main"
+
+.avatar
+.avatar__inner
+    position: relative
+    padding-top: 100%
+
+.avatar__img
+    border-radius: 50%
+    border: 1px solid $bg-dark
+    outline: 3px solid $bg-white
+    // outline-offset: 1px
+    position: absolute
+    inset: 0
+    background-position: center
+    background-size: cover
+    background-repeat: no-repeat
+</style>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import ProfileBackground from '@/components/layout/profile/ProfileBackground.vue';
-
+import ProfileAvatar from '@/components/layout/profile/ProfileAvatar.vue';
 </script>
 
 <template>
@@ -9,10 +9,28 @@ import ProfileBackground from '@/components/layout/profile/ProfileBackground.vue
     />
     <div class="container">
         <div class="main-inner">
-            profile {{ $route.params.username }}
+            <section class="profile__head">
+                <div class="profile__avatar">
+                    <ProfileAvatar 
+                        :img-url="'/marylin.jpg'"
+                    />
+                </div>
+                UserName
+            </section>
         </div>
     </div>
 </template>
 
 <style scoped lang="sass">
+@import "@/assets/styles/main"
+
+.profile__head
+    display: grid
+    grid-template-columns: 3fr 8fr
+    align-items: center
+    gap: 20px
+    text-align: center
+
+.profile__avatar
+    transform: translateY(-40%)
 </style>
